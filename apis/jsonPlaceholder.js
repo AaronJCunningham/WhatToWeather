a6606ff7328e016d87482d949f667483a7d31510
Aaron J. Cunningham <40688786+AaronJCunningham@users.noreply.github.com>
2021-03-23 12:44:11 +0100

    Update jsonPlaceholder.js

--- apis/jsonPlaceholder.js
+++ apis/jsonPlaceholder.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_KEY = "cf46012ab2c09f0441384cc2ff9e508c";
+const API_KEY = "yourkeyhere";
 
 export default axios.create({
   baseURL: `http://api.openweathermap.org/data/2.5/weather?lat=25&lon=43&APPID=${API_KEY}&units=metric`,
